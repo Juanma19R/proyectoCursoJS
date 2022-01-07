0480f6c7a38f22bff2671af8d58635fcef4855b1
Juanma19R <juan.ruggeri@hotmail.com>
2022-01-07 18:03:38 -0300

    Se agrego un array

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ saludo();
 
 class Streaming {
     constructor(titulo, genero, tipo) {
-    this.titulo = titulo;
-    this.genero = genero;
-    this.tipo = tipo;
-}
+        this.titulo = titulo;
+        this.genero = genero;
+        this.tipo = tipo;
+    }
 mostrarInfo() {
     alert(
         `${this.titulo} es del genero ${this.genero} y es un/una ${this.tipo}`
@@ -20,12 +20,17 @@ mostrarInfo() {
     }
 }
 
-const titulo = prompt("¿Que quieres ver hoy?");
-const genero = prompt("¿A que genero pertenece?");
-const tipo = prompt("¿Es una serie, un documental o una pelicula?");
-const instanciaStream = new Streaming(titulo, genero, tipo);
-instanciaStream.mostrarInfo();
+const arrayStream = [];
+let pregunta = prompt("¿Desea ver algo? (si/no)");
 
-document.write(
-    `Elegiste ver ${instanciaStream.titulo}`
-);
+while (pregunta === "si") {
+    let titulo = prompt("¿Que quieres ver hoy?");
+    let genero = prompt("¿A que genero pertenece?");
+    let tipo = prompt("¿Es una serie, un documental o una pelicula?");
+    const busquedaStreaming = new Streaming(titulo, genero, tipo);
+    busquedaStreaming.mostrarInfo();
+    arrayStream.push(busquedaStreaming);
+    console.log(arrayStream);
+    pregunta = prompt("¿desea ver otra cosa? (si/no)");
+    document.write(`Elegiste ver ${busquedaStreaming.titulo}`);
+}
