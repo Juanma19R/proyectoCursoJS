c6d4688e24ff333d68afa39d1ec17fa0773e2036
Juanma19R <juan.ruggeri@hotmail.com>
2022-01-17 20:00:19 -0300

    Se creo nuevamente el navbar a travez del DOM, agregando un titulo y un saludo al documento

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//PROGRAMA
+
 let nombre = prompt("Ingrese su nombre:"); //Se pide el nombre del usuario para dar una saludo.
 
 function saludo(nombre) { //Se crea una funcion con el nombre pedido en la variable "nombre"
@@ -33,3 +35,22 @@ while (pregunta === "si") {
     pregunta = prompt("¿desea ver otra cosa? (si/no)");
     document.write(`Elegiste ver ${busquedaStreaming.titulo}`);
 }
+
+//NAVBAR
+
+//Se toma el header del index y se guarda en una variable, luego se crea el elemento nav y se guarda en otra variable
+let header = document.getElementById("menuNav");
+let navBar = document.createElement("nav");
+
+navBar.classList.add("navbar", "navbar-expand-lg", "navbar-light", "bg-light"); //Se agregan las clases de bootstrap para el navBar
+navBar.innerHTML = 
+`<div class="container-fluid">
+    <a class="navbar-brand">
+        <img src="./img/tv.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+        Streaming House
+    </a>
+</div>`;
+header.appendChild(navBar); //A la variable "header" se le agrega el elemento que esta guardado en "navBar"
+
+let title = document.getElementById("title"); //Se toma el div con un ID y se agrega una etiqueta h1 para agregar un titulo
+title.innerHTML = `<h1>¡Bienvenid@ a Streaming House ${nombre}!</h1>`;
