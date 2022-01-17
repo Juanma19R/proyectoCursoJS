b0037fb87a4e52d380fa98e5aedc902f22f85368
Juanma19R <juan.ruggeri@hotmail.com>
2022-01-17 17:32:17 -0300

    Se realizaron las sugerencias dichas por el tutor en la ultima entrega del trabajo final

--- js/script.js
+++ js/script.js
@@ -1,30 +1,29 @@
-function saludo() {
-    alert(
-        `¡Bienvenid@ ${nombre}!`
-    );
+let nombre = prompt("Ingrese su nombre:"); //Se pide el nombre del usuario para dar una saludo.
+
+function saludo(nombre) { //Se crea una funcion con el nombre pedido en la variable "nombre"
+    let saludoUser = "Bienvenid@ " + nombre;
+    return saludoUser;
 }
 
-let nombre = prompt("Ingrese su nombre:");
-saludo();
+let saludoFinal = saludo(nombre);
+alert(saludoFinal); //Finalmente se ejecuta el saludo en un alert
 
-class Streaming {
+class Streaming { 
     constructor(titulo, genero, tipo) {
         this.titulo = titulo;
         this.genero = genero;
         this.tipo = tipo;
     }
-mostrarInfo() {
-    alert(
-        `${this.titulo} es del genero ${this.genero} y es un/una ${this.tipo}`
-        );
+    mostrarInfo() { //Luego de obtener los datos "titulo,genero y tipo" se imprime una alerta mostrando la información
+        alert(`${this.titulo} es del genero ${this.genero} y es un/una ${this.tipo}`);
     }
 }
 
 const arrayStream = [];
-let pregunta = prompt("¿Desea ver algo? (si/no)");
+let pregunta = prompt("¿Desea ver algo? (si/no)"); //Se crea un array con la información pedida si la respuesta es "si"
 
 while (pregunta === "si") {
-    let titulo = prompt("¿Que quieres ver hoy?");
+    let titulo = prompt("¿Que quieres ver hoy?"); 
     let genero = prompt("¿A que genero pertenece?");
     let tipo = prompt("¿Es una serie, un documental o una pelicula?");
     const busquedaStreaming = new Streaming(titulo, genero, tipo);
